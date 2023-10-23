6c8e20f7addbfb533047403e1f2f6ca93ad8d7ba
harmon1c <80057115+harmon1c@users.noreply.github.com>
2023-10-23 15:54:13 +0300

    feat: add numbers task solution 2023-10-23 15:53:48

--- test/01-strings-tests.js
+++ test/01-strings-tests.js
@@ -1,62 +1,51 @@
 const assert = require('assert');
 const tasks = require('../src/01-strings-tasks');
 it.optional = require('../extensions/it-optional');
-
 describe('01-strings-tasks', () => {
   it.optional('concatenateStrings should return concatenation of two strings', () => {
     assert.equal(tasks.concatenateStrings('aa', 'bb'), 'aabb');
     assert.equal(tasks.concatenateStrings('aa', ''), 'aa');
     assert.equal(tasks.concatenateStrings('', 'bb'), 'bb');
   });
-
   it.optional('getStringLength should return the length of string', () => {
     assert.equal(tasks.getStringLength('aaaaa'), 5, "'aaaaa' length should be 5");
     assert.equal(tasks.getStringLength(''), 0, "'' length should be 0");
   });
-
   it.optional('getStringFromTemplate should create a string from template using given parameters', () => {
     assert.equal(tasks.getStringFromTemplate('John', 'Doe'), 'Hello, John Doe!');
     assert.equal(tasks.getStringFromTemplate('Chuck', 'Norris'), 'Hello, Chuck Norris!');
   });
-
   it.optional('extractNameFromTemplate should parse the name from given string', () => {
     assert.equal(tasks.extractNameFromTemplate('Hello, John Doe!'), 'John Doe');
     assert.equal(tasks.extractNameFromTemplate('Hello, Chuck Norris!'), 'Chuck Norris');
   });
-
   it.optional('getFirstChar should return the first char from given string', () => {
     assert.equal(tasks.getFirstChar('John Doe'), 'J');
     assert.equal(tasks.getFirstChar('cat'), 'c');
   });
-
   it.optional('removeLeadingAndTrailingWhitespaces should remove leading and trailing whitespaces from the string', () => {
     assert.equal(tasks.removeLeadingAndTrailingWhitespaces('  Abracadabra'), 'Abracadabra');
     assert.equal(tasks.removeLeadingAndTrailingWhitespaces('cat'), 'cat');
     assert.equal(tasks.removeLeadingAndTrailingWhitespaces('\tHello, World! '), 'Hello, World!');
   });
-
   it.optional('repeatString should repeat string specified number of times', () => {
     assert.equal(tasks.repeatString('A', 5), 'AAAAA');
     assert.equal(tasks.repeatString('cat', 3), 'catcatcat');
   });
-
   it.optional('removeFirstOccurrences should remove all specified values from a string', () => {
     assert.equal(tasks.removeFirstOccurrences('To be or not to be', ' not'), 'To be or to be');
     assert.equal(tasks.removeFirstOccurrences('I like legends', 'end'), 'I like legs');
     assert.equal(tasks.removeFirstOccurrences('ABABAB', 'BA'), 'ABAB');
   });
-
   it.optional('unbracketTag should remove first and last angle brackets from tag string', () => {
     assert.equal(tasks.unbracketTag('<div>'), 'div');
     assert.equal(tasks.unbracketTag('<span>'), 'span');
     assert.equal(tasks.unbracketTag('<a>'), 'a');
   });
-
   it.optional('convertToUpperCase should convert all chars from specified string into upper case', () => {
     assert.equal(tasks.convertToUpperCase('Thunderstruck'), 'THUNDERSTRUCK');
     assert.equal(tasks.convertToUpperCase('abcdefghijklmnopqrstuvwxyz'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
   });
-
   it.optional('extractEmails should extract emails from string list delimeted by semicolons', () => {
     assert.deepEqual(
       tasks.extractEmails('angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com'),
@@ -67,7 +56,6 @@ describe('01-strings-tasks', () => {
       ['info@gmail.com'],
     );
   });
-
   it.optional('getRectangleString should return the string reprentation of rectangle with specified size', () => {
     assert.equal(
       tasks.getRectangleString(6, 4),
@@ -91,7 +79,6 @@ describe('01-strings-tasks', () => {
       + '└──────────┘\n',
     );
   });
-
   it.optional('encodeToRot13 should encode-decode string using ROT13 algorithm', () => {
     assert.equal(tasks.encodeToRot13('hello'), 'uryyb');
     assert.equal(tasks.encodeToRot13('Why did the chicken cross the road?'), 'Jul qvq gur puvpxra pebff gur ebnq?');
@@ -101,7 +88,6 @@ describe('01-strings-tasks', () => {
       'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm',
     );
   });
-
   it.optional('isString should return true if argument is a string', () => {
     assert.equal(tasks.isString(), false, 'undefined');
     assert.equal(tasks.isString(null), false, 'null');
@@ -110,7 +96,6 @@ describe('01-strings-tasks', () => {
     // eslint-disable-next-line no-new-wrappers
     assert.equal(tasks.isString(new String('test')), true, "new String('test')");
   });
-
   it.optional('getCardId should return the index of card in the initial deck', () => {
     [
       'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
